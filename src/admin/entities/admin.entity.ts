6979d5c05ef2aa1f0503a7e5b624af140de4bce1
kolladarshan <kolladarshan21@gmail.com>
2026-02-06 16:35:45 +0530

    Intial commit

---
+++ src/admin/entities/admin.entity.ts
@@ -0,0 +1,16 @@
+import { Roles } from "src/enums/rol-enum";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+@Entity('user')
+export class AdminEntity {
+    @PrimaryGeneratedColumn()
+    id:number;
+    @Column()
+    name:string;
+    @Column()
+    email:string;
+    @Column()
+    password:string;
+    @Column({type:'enum' ,enum:Roles, array:true,default:[Roles.ADMIN]})
+    roles:Roles
+
+}
