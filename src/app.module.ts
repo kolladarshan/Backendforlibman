9f87ab09bbb984103b6a38b6d83e95bd806299a4
kolladarshan <kolladarshan21@gmail.com>
2026-02-11 16:53:54 +0530

    middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { ,MiddlewareConsumer,Module,RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TypeOrmModule} from  '@nestjs/typeorm';
 import { dataSourceOptions } from './db/data-source';
 import { AdminModule } from './admin/admin.module';
+import { CurrentuserMiddleware } from './admin/middleware/user-utility';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -13,4 +14,12 @@ import { AdminModule } from './admin/admin.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  
+export class AppModule  {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CurrentuserMiddleware)
+      .forRoutes({path:'*',method : RequestMethod.ALL});
+  }
+}
